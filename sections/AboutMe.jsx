5e61fbc5178ecb10362f4e8451adcdfb3ca8bb0f
Rahat47 <djrayhan8@gmail.com>
2021-12-06 16:52:48 +0600

    updated about page picture

--- sections/AboutMe.jsx
+++ sections/AboutMe.jsx
@@ -90,9 +90,7 @@ export default function About() {
                     <Image
                         rounded={'md'}
                         alt={'A bad picture of Me'}
-                        src={
-                            'https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-                        }
+                        src={'/images/site-image.jpg'}
                         objectFit={'cover'}
                     />
                 </Flex>
